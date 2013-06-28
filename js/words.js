978a396a3896b123f638483a66b17549e88885cf
Evan Hahn <me@evanhahn.com>
2013-06-28 10:04:14 +0700

    add all the words

--- js/words.js
+++ js/words.js
@@ -39,4 +39,941 @@ window.words = [
 		]
 	},
 
+	{
+		word: "ante",
+		definitions: [
+			"n: difference",
+			"mod: different",
+			"conj: otherwise, or else",
+			"vt: change, alter, modify"
+		]
+	},
+
+	{
+		word: "anu",
+		definitions: [
+			"conj: or"
+		]
+	},
+
+	{
+		word: "awen",
+		definitions: [
+			"vi: stay, wait, remain",
+			"vt: keep",
+			"mod: remaining, stationary, permanent, sedentary"
+		]
+	},
+
+	{
+		word: "e",
+		definitions: [
+			"sep: (introduces a direct object)"
+		]
+	},
+
+	{
+		word: "en",
+		definitions: [
+			"conj: and (used to coordinate head nouns)"
+		]
+	},
+
+	{
+		word: "esun",
+		definitions: [
+			"n: market, shop"
+		]
+	},
+
+	{
+		word: "ijo",
+		definitions: [
+			"n: thing, something, stuff, anything, object",
+			"mod: of something",
+			"vt: objectify"
+		]
+	},
+
+	{
+		word: "ike",
+		definitions: [
+			"mod: bad, negative, wrong, evil, overly complex, (figuratively) unhealthy",
+			"interj: oh dear! woe! alas!",
+			"n: negativity, badness, evil",
+			"vt: to make bad, to worsen, to have a negative effect upon",
+			"vi: to be bad, to suck"
+		]
+	},
+
+	{
+		word: "ilo",
+		definitions: [
+			"n: tool, device, machine, thing used for a specific purpose"
+		]
+	},
+
+	{
+		word: "insa",
+		definitions: [
+			"n: inside, inner world, centre, stomach",
+			"mod: inner, internal"
+		]
+	},
+
+	{
+		word: "jaki",
+		definitions: [
+			"mod: dirty, gross, filthy",
+			"n: dirt, pollution, garbage, filth",
+			"vt: pollute, dirty",
+			"interj: ew! yuck!"
+		]
+	},
+
+	{
+		word: "jan",
+		definitions: [
+			"n: person, people, human, being, somebody, anybody",
+			"mod: human, somebody's, personal, of people",
+			"vt: personify, humanize, personalize"
+		]
+	},
+
+	{
+		word: "jelo",
+		definitions: [
+			"mod: yellow, light green"
+		]
+	},
+
+	{
+		word: "jo",
+		definitions: [
+			"vt: have, contain",
+			"n: having",
+			"kama: receive, get, take, obtain"
+		]
+	},
+
+	{
+		word: "kala",
+		definitions: [
+			"n: fish, sea creature"
+		]
+	},
+
+	{
+		word: "kalama",
+		definitions: [
+			"n: sound, noise, voice",
+			"vi: make noise",
+			"vt: sound, ring, play (an instrument)"
+		]
+	},
+
+	{
+		word: "kama",
+		definitions: [
+			"vi: come, become, arrive, happen, pursue actions to arrive to (a certain state), manage to, start to",
+			"n: event, happening, chance, arrival, beginning",
+			"mod: coming, future",
+			"vt: bring about, summon"
+		]
+	},
+
+	{
+		word: "kasi",
+		definitions: [
+			"n: plant, leaf, herb, tree, wood"
+		]
+	},
+
+	{
+		word: "ken",
+		definitions: [
+			"vi: can, is able to, is allowed to, may, is possible",
+			"n: possibility, ability, power to do things, permission",
+			"vt: make possible, enable, allow, permit",
+			"cont: it is possible that",
+		]
+	},
+
+	{
+		word: "kepeken",
+		definitions: [
+			"vt: use",
+			"prep: with"
+		]
+	},
+
+	{
+		word: "kili",
+		definitions: [
+			"n: fruit, pulpy vegetable, mushroom"
+		]
+	},
+
+	{
+		word: "kin",
+		definitions: [
+			"mod: also, too, even, indeed (emphasizes the word(s) before it)"
+		]
+	},
+
+	{
+		word: "kiwen",
+		definitions: [
+			"mod: hard, solid, stone-like, made of stone or metal",
+			"n: hard thing, rock, stone, metal, mineral, clay"
+		]
+	},
+
+	{
+		word: "ko",
+		definitions: [
+			"n: semi-solid or squishy substance, e.g. paste, powder, gum"
+		]
+	},
+
+	{
+		word: "kon",
+		definitions: [
+			"n: air, wind, smell, soul",
+    	"mod: air-like, ethereal, gaseous"
+		]
+	},
+
+	{
+		word: "kule",
+		definitions: [
+			"n: colour, paint",
+			"mod: colourful",
+			"vt: colour, paint"
+		]
+	},
+
+	{
+		word: "kute",
+		definitions: [
+			"vt: listen, hear",
+			"mod: auditory, hearing"
+		]
+	},
+
+	{
+		word: "kulupu",
+		definitions: [
+			"n: group, community, society, company, people",
+			"mod: communal, shared, public, of the society"
+		]
+	},
+
+	{
+		word: "la",
+		definitions: [
+			"sep: (between adverb or phrase of context and sentence)"
+		]
+	},
+
+	{
+		word: "lape",
+		definitions: [
+			"n, vi: sleep, rest",
+			"mod: sleeping, of sleep"
+		]
+	},
+
+	{
+		word: "laso",
+		definitions: [
+			"mod: blue, blue-green"
+		]
+	},
+
+	{
+		word: "lawa",
+		definitions: [
+			"n: head, mind",
+			"mod: main, leading, in charge",
+			"vt: lead, control, rule, steer"
+		]
+	},
+
+	{
+		word: "len",
+		definitions: [
+			"n: lothing, cloth, fabric"
+		]
+	},
+
+	{
+		word: "lete",
+		definitions: [
+			"n: cold",
+			"mod: cold, uncooked",
+			"vt: cool down, chill"
+		]
+	},
+
+	{
+		word: "li",
+		definitions: [
+			"sep: (between any subject except mi and sina and its verb; also used to introduce a new verb for the same subject)"
+		]
+	},
+
+	{
+		word: "lili",
+		definitions: [
+			"mod: small, little, young, a bit, short, few, less",
+			"vt: reduce, shorten, shrink, lessen"
+		]
+	},
+
+	{
+		word: "linja",
+		definitions: [
+			"n: long, very thin, floppy thing, e.g. string, rope, hair, thread, cord, chain"
+		]
+	},
+
+	{
+		word: "lipu",
+		definitions: [
+			"n: flat and bendable thing, e.g. paper, card, ticket"
+		]
+	},
+
+	{
+		word: "loje",
+		definitions: [
+			"mod: red"
+		]
+	},
+
+	{
+		word: "lon",
+		definitions: [
+			"prep: be (located) in/at/on",
+			"vi: be there, be present, be real/true, exist, be awake"
+		]
+	},
+
+	{
+		word: "luka",
+		definitions: [
+			"n: hand, arm"
+		]
+	},
+
+	{
+		word: "lukin",
+		definitions: [
+			"vt: see, look at, watch, read",
+			"vi: look, watch out, pay attention",
+			"mod: visual(ly)"
+		]
+	},
+
+	{
+		word: "lupa",
+		definitions: [
+			"n: hole, orifice, window, door"
+		]
+	},
+
+	{
+		word: "ma",
+		definitions: [
+			"n: land, earth, country, (outdoor) area"
+		]
+	},
+
+	{
+		word: "mama",
+		definitions: [
+			"n: parent, mother, father",
+			"mod: of the parent, parental, maternal, fatherly"
+		]
+	},
+
+	{
+		word: "mani",
+		definitions: [
+			"n: money, material wealth, currency, dollar, capital"
+		]
+	},
+
+	{
+		word: "meli",
+		definitions: [
+			"n: woman, female, girl, wife, girlfriend",
+			"mod: female, feminine, womanly"
+		]
+	},
+
+	{
+		word: "mi",
+		definitions: [
+			"n: I, we",
+			"mod: my, our"
+		]
+	},
+
+	{
+		word: "mije",
+		definitions: [
+			"n: man, male, boy, husband, boyfriend",
+			"mod: male, masculine, manly"
+		]
+	},
+
+	{
+		word: "moku",
+		definitions: [
+			"n: food, meal",
+			"vt: eat, drink, swallow, ingest, consume"
+		]
+	},
+
+	{
+		word: "moli",
+		definitions: [
+			"n: death",
+			"vi: die, be dead",
+			"vt: kill",
+			"mod: dead, deadly, fatal"
+		]
+	},
+
+	{
+		word: "monsi",
+		definitions: [
+			"n: back, rear end, butt, behind",
+			"mod: back, rear"
+		]
+	},
+
+	{
+		word: "mu",
+		definitions: [
+			"interj: woof! meow! moo! etc. (cute animal noise)"
+		]
+	},
+
+	{
+		word: "mun",
+		definitions: [
+			"n: moon",
+			"mod: lunar"
+		]
+	},
+
+	{
+		word: "musi",
+		definitions: [
+			"n: fun, playing, game, recreation, art, entertainment",
+			"mod: artful, fun, recreational",
+			"vi: play, have fun",
+			"vt: amuse, entertain"
+		]
+	},
+
+	{
+		word: "mute",
+		definitions: [
+			"mod: many, very, much, several, a lot, abundant, numerous, more",
+			"n: amount, quantity",
+			"vt: make many or much"
+		]
+	},
+
+	{
+		word: "namako",
+		definitions: [
+			"mod: extra, additional, spice"
+		]
+	},
+
+	{
+		word: "nanpa",
+		definitions: [
+			"n: number",
+			"oth: -th (ordinal numbers)"
+		]
+	},
+
+	{
+		word: "nasa",
+		definitions: [
+			"mod: silly, crazy, foolish, drunk, strange, stupid, weird",
+			"vt: drive crazy, make weird"
+		]
+	},
+
+	{
+		word: "nasin",
+		definitions: [
+			"n: way, manner, custom, road, path, doctrine, system, method"
+		]
+	},
+
+	{
+		word: "nena",
+		definitions: [
+			"n: bump, nose, hill, mountain, button"
+		]
+	},
+
+	{
+		word: "ni",
+		definitions: [
+			"mod: this, that"
+		]
+	},
+
+	{
+		word: "nimi",
+		definitions: [
+			"n: word, name"
+		]
+	},
+
+	{
+		word: "noka",
+		definitions: [
+			"n: leg, foot"
+		]
+	},
+
+	{
+		word: "o",
+		definitions: [
+			"sep: O (vocative or imperative)",
+			"interj: hey! (calling somebody's attention)"
+		]
+	},
+
+	{
+		word: "oko",
+		definitions: [
+			"n: eye"
+		]
+	},
+
+	{
+		word: "olin",
+		definitions: [
+			"n: love",
+			"mod: love",
+			"vt: to love (a person)"
+		]
+	},
+
+	{
+		word: "ona",
+		definitions: [
+			"n: she, he, it, they",
+			"mod: her, his, its, their"
+		]
+	},
+
+	{
+		word: "open",
+		definitions: [
+			"vt: open, turn on"
+		]
+	},
+
+	{
+		word: "pakala",
+		definitions: [
+			"n: blunder, accident, mistake, destruction, damage, breaking",
+			"vt: screw up, fuck up, botch, ruin, break, hurt, injure, damage, spoil, ruin",
+			"vi: screw up, fall apart, break",
+			"interj: damn! fuck!"
+		]
+	},
+
+	{
+		word: "pali",
+		definitions: [
+			"n: activity, work, deed, project",
+			"mod: active, work-related, operating, working",
+			"vt: do, make, build, create",
+			"vi: act, work, function"
+		]
+	},
+
+	{
+		word: "palisa",
+		definitions: [
+			"n: long, mostly hard object, e.g. rod, stick, branch"
+		]
+	},
+
+	{
+		word: "pan",
+		definitions: [
+			"n: grain, cereal"
+		]
+	},
+
+	{
+		word: "pana",
+		definitions: [
+			"vt: give, put, send, place, release, emit, cause",
+			"n: giving, transfer, exchange"
+		]
+	},
+
+	{
+		word: "pi",
+		definitions: [
+			"sep: of, belonging to"
+		]
+	},
+
+	{
+		word: "pilin",
+		definitions: [
+			"n: feelings, emotion, heart",
+			"vi: feel",
+			"vt: feel, think, sense, touch"
+		]
+	},
+
+	{
+		word: "pimeja",
+		definitions: [
+			"mod: black, dark",
+			"n: darkness, shadows",
+			"vt: darken"
+		]
+	},
+
+	{
+		word: "pini",
+		definitions: [
+			"n: end, tip",
+			"mod: completed, finished, past, done, ago",
+			"vt: finish, close, end, turn off"
+		]
+	},
+
+	{
+		word: "pipi",
+		definitions: [
+			"n: bug, insect, spider"
+		]
+	},
+
+	{
+		word: "poka",
+		definitions: [
+			"n: side, hip, next to",
+			"prep: in the accompaniment of, with",
+			"mod: neihbouring"
+		]
+	},
+
+	{
+		word: "poki",
+		definitions: [
+			"n: container, box, bowl, cup, glass"
+		]
+	},
+
+	{
+		word: "pona",
+		definitions: [
+			"n: good, simplicity, positivity",
+			"mod: good, simple, positive, nice, correct, right",
+			"interj: great! good! thanks! OK! cool! yay!",
+			"vt: improve, fix, repair, make good"
+		]
+	},
+
+	{
+		word: "sama",
+		definitions: [
+			"mod: same, similar, equal, of equal status or position",
+			"prep: like, as, seem"
+		]
+	},
+
+	{
+		word: "seli",
+		definitions: [
+			"n: fire, warmth, heat",
+			"mod: hot, warm, cooked",
+			"vt: heat, warm up, cook"
+		]
+	},
+
+	{
+		word: "selo",
+		definitions: [
+			"n: outside, surface, skin, shell, bark, shape, peel"
+		]
+	},
+
+	{
+		word: "seme",
+		definitions: [
+			"oth: what, which, wh- (question word)"
+		]
+	},
+
+	{
+		word: "sewi",
+		definitions: [
+			"n: high, up, above, top, over, on",
+			"mod: superior, elevated, religious, formal"
+		]
+	},
+
+	{
+		word: "sijelo",
+		definitions: [
+			"n: body, physical state"
+		]
+	},
+
+	{
+		word: "sike",
+		definitions: [
+			"n: circle, wheel, sphere, ball, cycle",
+			"mod: round, cyclical"
+		]
+	},
+
+	{
+		word: "sin",
+		definitions: [
+			"mod: new, fresh, another, more",
+			"vt: renew, renovate, freshen"
+		]
+	},
+
+	{
+		word: "sina",
+		definitions: [
+			"n: you",
+			"mod: your"
+		]
+	},
+
+	{
+		word: "sinpin",
+		definitions: [
+			"n: front, chest, torso, face, wall"
+		]
+	},
+
+	{
+		word: "sitelen",
+		definitions: [
+			"n: picture, image",
+			"vt: draw, write"
+		]
+	},
+
+	{
+		word: "sona",
+		definitions: [
+			"n: knowledge, wisdom, intelligence, understanding",
+			"vt: know, understand, know how to",
+			"vi: know, understand",
+			"kama: learn, study"
+		]
+	},
+
+	{
+		word: "soweli",
+		definitions: [
+			"n: animal, especially land mammal, lovable animal"
+		]
+	},
+
+	{
+		word: "suli",
+		definitions: [
+			"mod: big, tall, long, adult, important",
+			"vt: enlarge, lengthen",
+			"n: size"
+		]
+	},
+
+	{
+		word: "suno",
+		definitions: [
+			"n: sun, light"
+		]
+	},
+
+	{
+		word: "supa",
+		definitions: [
+			"n: horizontal surface, e.g furniture, table, chair, pillow, floor"
+		]
+	},
+
+	{
+		word: "suwi",
+		definitions: [
+			"n: candy, sweet food",
+			"mod: sweet, cute",
+			"vt: sweeten"
+		]
+	},
+
+	{
+		word: "tan",
+		definitions: [
+			"prep: from, by, because of, since",
+			"n: origin, cause"
+		]
+	},
+
+	{
+		word: "taso",
+		definitions: [
+			"mod: only, sole",
+			"conj: but"
+		]
+	},
+
+	{
+		word: "tawa",
+		definitions: [
+			"prep: to, in order to, towards, for, until",
+			"vi: go to, walk, travel, move, leave",
+			"n: movement, transportation",
+			"mod: moving, mobile",
+			"vt: move, displace"
+		]
+	},
+
+	{
+		word: "telo",
+		definitions: [
+			"n: water, liquid, juice, sauce",
+			"vt: water, wash with water"
+		]
+	},
+
+	{
+		word: "tenmp",
+		definitions: [
+			"n: time, period of time, moment, duration, situation"
+		]
+	},
+
+	{
+		word: "toki",
+		definitions: [
+			"n: language, talking, speech, communication",
+			"mod: talking, verbal",
+			"vt: say",
+			"vi: talk, chat, communicate",
+			"interj: hello! hi!"
+		]
+	},
+
+	{
+		word: "tomo",
+		definitions: [
+			"n: indoor constructed space, e.g. house, home, room, building",
+			"mod: urban, domestic, household"
+		]
+	},
+
+	{
+		word: "tu",
+		definitions: [
+			"mod: two",
+			"n: duo, pair",
+			"vt: double, separate/cut/divide in two"
+		]
+	},
+
+	{
+		word: "unpa",
+		definitions: [
+			"n: sex, sexuality",
+			"mod: erotic, sexual",
+			"vt: have sex with, sleep with, fuck",
+			"vi: have sex"
+		]
+	},
+
+	{
+		word: "uta",
+		definitions: [
+			"n: mouth",
+			"mod: oral"
+		]
+	},
+
+	{
+		word: "utala",
+		definitions: [
+			"n: conflict, disharmony, competition, fight, war, battle, attack, blow, argument, physical or verbal violence",
+			"vt: hit, strike, attack, compete against"
+		]
+	},
+
+	{
+		word: "walo",
+		definitions: [
+			"mod: white, light (colour)",
+			"n: white thing or part, whiteness, lightness"
+		]
+	},
+
+	{
+		word: "wan",
+		definitions: [
+			"mod: one, a",
+			"n: unit, element, particle, part, piece",
+			"vt: unite, make one"
+		]
+	},
+
+	{
+		word: "waso",
+		definitions: [
+			"n: bird, winged animal"
+		]
+	},
+
+	{
+		word: "wawa",
+		definitions: [
+			"n: energy, strength, power",
+			"mod: energetic, strong, fierce, intense, sure, confident",
+			"vt: strengthen, energize, empower"
+		]
+	},
+
+	{
+		word: "weka",
+		definitions: [
+			"mod: away, absent, missing",
+			"n: absence",
+			"vt: throw away, remove, get rid of"
+		]
+	},
+
+	{
+		word: "wile",
+		definitions: [
+			"vt: to want, need, wish, have to, must, will, should",
+			"n: desire, need, will",
+			"mod: necessary"
+		]
+	}
+
 ];
